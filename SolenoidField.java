3faf9130589d8d58c6da84fda19683af1a771167
A-Hubbell <austin.hubbell@gmail.com>
2014-03-21 14:13:18 -0400

    Added solenoid magnetic field program
    “solenoidRadius” was actually the diameter, had to be cut in half

--- SolenoidField.java
+++ SolenoidField.java
@@ -1,4 +1,4 @@
-//Program for calculating the magnetic field of a solenoid
+//Basic program for estimating the magnetic field inside a solenoid
 
 
 import java.text.*;
@@ -13,23 +13,23 @@ public class SolenoidField
 		NumberFormat formatter = new DecimalFormat("0.00000E0");
 
 		//Properties of wire
-		double wireRadius = 0.001295;  //2.59 mm diameter for 10 AWG wire
+		double wireRadius = 0.000725;  //1.45 mm diameter/0.725 mm radius for 15 AWG wire
 		double wireLength;
 		double wireResistivity = 1.68*(Math.pow(10.0, -8.0)); //1.68 e-8 for copper
 		double wireResistance;
 		double copperVolume;
 		
 		//Solenoid properties
-		double solenoidRadius = 0.06985 + wireRadius;
+		double solenoidRadius = 0.034925 + wireRadius;
 		double solenoidLength = 0.15;
 		int spacingError = 1;  //estimated number of unused loop space (in number of loops) due to manufacturing/winding error
 		double solenoidBField;
 		
-		//Particle properties
+		//Ion properties
 		double particleSpeed = 2.7*(Math.pow(10, 8));   //Assuming a speed of 270 000 km/s
 		double particleCharge = 1.602176487*(Math.pow(10.0, -19.0));
 		double particleMass = 9.10938215*(Math.pow(10.0, -31));
-		double maxParticleCurvature;		
+		double minParticleCurvature;		
 				
 		//Misc variables
 		double permFreeSpace = (4*Math.PI)*(Math.pow(10.0, -7.0));
@@ -55,13 +55,14 @@ public class SolenoidField
 		
 		//Maximum radius of curvature of electron/positron (assuming the angle between B and v is 90)
 		
-		maxParticleCurvature = (particleMass*particleSpeed)/(particleCharge*solenoidBField);
+		minParticleCurvature = (particleMass*particleSpeed)/(particleCharge*solenoidBField);
 		
-		System.out.printf("The minimum radius of curvature of electron/positron is %f m\n", maxParticleCurvature);
+		System.out.printf("The minimum radius of curvature of electron/positron is %f m\n", minParticleCurvature);
 		
 		copperVolume = wireLength*(Math.PI*wireRadius*wireRadius);
 		
-		System.out.printf("Volume of copper required: %s", formatter.format(copperVolume));
+		System.out.printf("Volume of copper required: %s\n", formatter.format(copperVolume));
+		System.out.println("Mass of copper required: " + copperVolume*8960);
 		
 	}
 
